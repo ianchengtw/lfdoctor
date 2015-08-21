4d1948d78168799e115461ad43667de31ac7512c
Ian <ian@Ians-MacBook-Pro.local>
2015-08-22 07:18:39 +0800

    use the root_path

--- resources/views/jquery.blade.php
+++ resources/views/jquery.blade.php
@@ -7,12 +7,13 @@
 </head>
 <body>
 	<h1>jQuery testing</h1>
+	<p></p>
 	<script>
 		$(document).ready(function() {
-			$.post("http://homestead.app/itembyid/3", {}, function(data){console.log(data)});
-			$.post("http://homestead.app/itembyname/陳靜婉", {}, function(data){console.log(data)});
-			$.post("http://homestead.app/itembylatlng/22.757419580292/121.15435723029", {}, function(data){console.log(data)});
-			$.post("http://homestead.app/itemsincircle/22.757419580292/121.15435723029/5", {}, function(data){console.log(data)});
+			$.post('{{ URL::to("itembyid/3") }}', {}, function(data){console.log(data)});
+			$.post('{{ URL::to("itembyname/陳靜婉") }}', {}, function(data){console.log(data)});
+			$.post('{{ URL::to("itembylatlng/22.757419580292/121.15435723029") }}', {}, function(data){console.log(data)});
+			$.post('{{ URL::to("itemsincircle/22.757419580292/121.15435723029/5") }}', {}, function(data){console.log(data)});
 		})
 	</script>
 </body>
